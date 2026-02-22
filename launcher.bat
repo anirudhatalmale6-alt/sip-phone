5310d077aef9a489e12f7088f6f962df4c7b2b74
Anirudha Talmale <anirudhatalmale6@gmail.com>
2026-02-22 15:13:26 +0000

    Add launcher.bat to capture error output on crash

---
+++ launcher.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Starting SIP Phone...
+echo.
+cd /d "%~dp0"
+SIP-Phone.exe
+echo.
+echo ==========================================
+echo Program exited. If you see an error above,
+echo please copy-paste it and send it to me.
+echo ==========================================
+echo.
+pause
